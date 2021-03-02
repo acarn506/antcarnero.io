ce74b264927f7adaaf91f93f3b61dbba1a2a3420
acarn506 <acarnero@horizon.csueastbay.edu>
2021-03-01 23:04:25 -0800

    include intro

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,17 @@ import Container from "../components/Container/Container";
 export default function Home() {
   return (
     <Container>
-      <h1>Home</h1>
+      <section className="intro">
+        <h3 className="welcome">Welcome, I'm</h3>
+        <h1 className="name">Anthony Carnero</h1>
+        <h3>
+          I write code, create user-friendly interfaces, and visualize data.
+          <br />
+          Currently, searching for a new role as a Full-Stack or Frontend
+          Enginner.
+        </h3>
+        <h3>Checkout some of my visualzations and dashboards down below.</h3>
+      </section>
     </Container>
   );
 }
